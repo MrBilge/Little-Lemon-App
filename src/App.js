d48adca0c4f65028a8704f5f3cd8d111acad5743
MrBilge <b.eybek9307@gmail.com>
2024-06-26 14:31:51 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div>
-    <h3>Little Lemon</h3>
-  </div>;
+  return (
+    <div>
+      <h3>Little Lemonsss</h3>
+    </div>
+  );
 }
 
 export default App;
